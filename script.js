39064cfcd437c7d14dc073910bd99cb1afddb165
David <dcm2804@gmail.com>
2026-09-03 11:10:44 +0200

    Primer commit - web CIE Sara

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.contact-form');
+
+  if (form) {
+    form.setAttribute('data-ready', 'true');
+  }
+});
